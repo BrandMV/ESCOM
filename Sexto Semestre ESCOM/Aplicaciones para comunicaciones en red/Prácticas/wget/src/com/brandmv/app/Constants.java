fc815658234221def822851087624a82058544f4
BrandMV <brandonmv2001@gmail.com>
2022-05-22 19:10:44 -0500

    22-05-2022

---
+++ Sexto Semestre ESCOM/Aplicaciones para comunicaciones en red/Prácticas/wget/src/com/brandmv/app/Constants.java
@@ -0,0 +1,13 @@
+package com.brandmv.app;
+
+public class Constants {
+
+    public static final int BUFFER_SIZE = 65535;
+    public static final String DOWNLOAD_MESSAGE = "Se ha descargado el %%0.4f del archivo";
+    public static final String DOWNLOAD_FILE_MESSAGE = "Descargando el archivo %s";
+    public static final String ENTER_LINK_MESSAGE = "Ingrese el link del que desea descargar su contenido";
+    public static final String FILE_ERROR = "Ha ocurrido un error, el archivo %s no se pudo reonocer";
+    public static final String DOWNLOAD_ANOTHER_MESSAGE = "\n¿Desea ingresar otro link? Si/No";
+    public static final int THREADS = 10;
+
+}
