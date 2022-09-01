4bd6728e396ec9c2650be188090cd31a73d04826
BrandMV <brandonmv2001@gmail.com>
2022-09-01 01:18:09 -0500

    01-09-22

--- Septimo Semestre ESCOM/Desarrollo de sistemas distribuidos/Tareas/Tarea 1 - Primo/Programa/src/Primo.java
+++ Septimo Semestre ESCOM/Desarrollo de sistemas distribuidos/Tareas/Tarea 1 - Primo/Programa/src/Primo.java
@@ -2,82 +2,73 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 public class Primo {
     static Object obj = new Object();
     static int isPrime = 0;
-    static class Worker extends Thread{
+
+    static class Worker extends Thread {
         Socket conexion;
-        Worker(Socket conexion){
+
+        Worker(Socket conexion) {
             /**Desambiguamos y asignamos*/
             this.conexion = conexion;
         }
 
-        public void run(){
+        public void run() {
             /**Código despues de aceptar la conexion*/
             try {
                 DataOutputStream salida = new DataOutputStream(conexion.getOutputStream());
                 DataInputStream entrada = new DataInputStream(conexion.getInputStream());
                 System.out.println("Dentro hilo");
+                String verificacion = null;
+                /**Leyendo números*/
+                long numero = entrada.readLong();
+                long numeroInicial = entrada.readLong();
+                long numeroFinal = entrada.readLong();
+               // System.out.println("Intervalo: " + numeroInicial + ", " + numeroFinal + ", numero: " + numero);
 
-                synchronized (obj){
-                    /**Leyendo números*/
-                    long numero = entrada.readLong();
-                    long numeroInicial = entrada.readLong();
-                    long numeroFinal = entrada.readLong();
-
-                    System.out.println("Intervalo: " + numeroInicial +  ", " + numeroFinal);
-
-
-                    for(long i = numeroInicial; i < numeroFinal; i++){
-                        if(numero % i == 0){
+                // Se sincroniza el proceso que modifica la variable verificacion la cual indica al servidor b si se encontro un numero que divide
+                synchronized (obj) {
+                    for (long i = numeroInicial; i <= numeroFinal; i++) {
+                        if (numero % i == 0) {
+                            System.out.println("la i: " + i);
                             isPrime++;
-                            salida.writeInt(isPrime);
-                            System.out.println("No es primo");
+                            verificacion = "DIVIDE";
+                            salida.writeInt(verificacion.length());
+                            salida.write(verificacion.getBytes(StandardCharsets.UTF_8));
+                            System.out.println("DIVIDE");
                             break;
                         }
                     }
+                    if (isPrime == 0) {
+                        verificacion = "NO DIVIDE";
+                        salida.writeInt(verificacion.length());
+                        salida.write(verificacion.getBytes(StandardCharsets.UTF_8));
+                        System.out.println("NO DIVIDE");
+                    }
                 }
-
-
-                if(isPrime == 0){
-                    salida.writeInt(isPrime);
-                    System.out.println("Es primo");
-                }
-
                 entrada.close();
                 salida.close();
                 conexion.close();
-            }
-            catch (Exception ex){
+            } catch (Exception ex) {
                 System.err.println(ex.getMessage());
-            }
-        }
-
-        public static void read(DataInputStream f, byte[] b, int posicion, int longitud) throws Exception{
-            while(longitud > 0){
-                int n = f.read(b, posicion, longitud);
-                posicion += n;
-                longitud -= n;
             }
         }
     }
 
-    public static void main(String[] args)throws Exception{
-        if(args.length != 1){
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
             System.out.println("Debe ingresar un socket como argumento, ejemplo: java Primo 50000");
             System.exit(0);
         }
         int port = Integer.valueOf(args[0]);
         ServerSocket servidor = new ServerSocket(port);
-        //for(int i=0; i < 4; i++){
-            Socket conexion = servidor.accept();
-            System.out.println("Conectamos");
-
-            Worker w = new Worker(conexion);
-            w.start();
-            w.join();
-        //}
+        Socket conexion = servidor.accept();
+        Worker w = new Worker(conexion);
+        w.start();
+        // Esperamos por la finalizacion del hilo
+        w.join();
     }
 }
--- Septimo Semestre ESCOM/Desarrollo de sistemas distribuidos/Tareas/Tarea 1 - Primo/Programa/src/ServidorHttp.java
+++ Septimo Semestre ESCOM/Desarrollo de sistemas distribuidos/Tareas/Tarea 1 - Primo/Programa/src/ServidorHttp.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -5,115 +6,98 @@ import java.util.Date;
 import java.util.StringTokenizer;
 
 public class ServidorHttp {
-    static class Worker extends Thread{
+    static class Worker extends Thread {
         Socket conexion;
-        Worker(Socket conexion){
+
+        Worker(Socket conexion) {
             this.conexion = conexion;
         }
-        public void run(){
-            //System.out.println((InetSocketAddress)conexion.g);
+
+        public void run() {
             try {
                 BufferedReader entrada = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
                 /**Con printwriter tiene los metodos para salto de linea*/
                 PrintWriter salida = new PrintWriter(conexion.getOutputStream());
-                // StringBuffer sb = new StringBuffer();
-
                 String s = entrada.readLine();
-                //System.out.println(s);
+                String verificacion = null;
 
-                if(s.startsWith("GET /primo?")){
-
-                    long numero = Long.parseLong(getNumero(s));
+                if (s.startsWith("GET /primo?")) {
+                    long numero = Long.parseLong(getNumero(s)) / 2;
 
                     /**Obteniendo los intervalos para mandar al Servidor A*/
                     long numeroInicial = 2;
                     long numeroFinal = 0;
 
                     int port = 50000;
-                    int conexiones = 0;
-                    Socket conexion[] = new Socket[4];
+                    Socket conexion = null;
+                    int primo = 0;
 
                     /**Nos conectamos a las 4 instancias del servidor A con un puerto diferente*/
-                    for(;;){
-                        try{
-                            System.out.println("Antes de crear socket");
-                            conexion[conexiones] = new Socket("Localhost", port);
-                            System.out.println("Despues de creear hilo");
+                    for (int i = 0; i < 4; i++) {
+                        try {
+                            //Creamos una instancia del servidor A
+                            conexion = new Socket("Localhost", port);
                             port++;
-                            conexiones++;
 
-                            System.out.println("Conexiones" + conexiones);
-                            if(conexiones == 4){
-                                break;
+                            /**Mandamos intervalos para que las instancias del servidor A hagan sus operaciones*/
+                            DataOutputStream salidaSocket = new DataOutputStream(conexion.getOutputStream());
+                            DataInputStream entradaSocket = new DataInputStream(conexion.getInputStream());
+                            if (i != 0) {
+                                numeroInicial = numeroFinal + 1;
                             }
+                            numeroFinal = (numero / 4) * (i + 1);
 
-                        }catch (Exception e){
+                            salidaSocket.writeLong(numero);
+                            salidaSocket.writeLong(numeroInicial);
+                            salidaSocket.writeLong(numeroFinal);
+
+                            int size = entradaSocket.readInt();
+                            byte[] buffer = new byte[size];
+                            System.out.println(size);
+                            read(entradaSocket, buffer, 0, size);
+                            verificacion = new String(buffer, "UTF-8");
+
+                            System.out.println("Intervalo " + (i + 1) + ": " + numeroInicial + ", " + numeroFinal + ", divide o no: " + verificacion + " tam: " + size);
+
+                            // Vamos sumando las veces que se obtuvo un DIVIDE de parte de los servidores
+                            if (verificacion.equals("DIVIDE")) primo++;
+
+                            salidaSocket.close();
+                            entradaSocket.close();
+                            conexion.close();
+                        } catch (Exception e) {
                             Thread.sleep(200);
                             System.err.println(e.getMessage());
                         }
                     }
 
-                    /**Mandamos intervalos para que las instancias del servidor A hagan sus operaciones*/
-                    for(int i = 0; i < 4; i++){
-                        System.out.println("Mandando intervalos");
-                        DataOutputStream salidaSocket = new DataOutputStream(conexion[i].getOutputStream());
-                        DataInputStream entradaSocket = new DataInputStream(conexion[i].getInputStream());
-
-                        if(i != 0){
-                            numeroInicial = numeroFinal + 1;
-                        }
-                        numeroFinal = (numero / 4) * (i + 1);
-                        System.out.println("Numero final en intervalo " + (i+1) + ": " + numeroFinal);
-                        if(numeroFinal == numero){
-                            System.out.println("Iguales");
-                            numeroFinal--;
-                        }
-
-                        salidaSocket.writeLong(numero);
-                        salidaSocket.writeLong(numeroInicial);
-                        salidaSocket.writeLong(numeroFinal);
-
-                        System.out.println("Intervalo " + (i+1) + ": " + numeroInicial +  ", " + numeroFinal);
-
-
-                        if(entradaSocket.readInt() > 0){
-                            System.out.println("Numero " + numero + "no es primo");
-                            String respuesta = "<html><h1>El número " + numero + " no es primo</h1>></html>";
-
-                            salida.println("HTTP/1.1 200 OK");
-                            salida.println("Server: Primo");
-                            salida.println("Date: " + new Date());
-                            salida.println("Content-type: text/html; charset=utf-8");
-                            salida.println("Content-length: " + respuesta.length());
-                            salida.println();
-                            salida.flush();//Garantizamos que el encabezado va
-                            salida.println(respuesta);
-                            salida.flush();
-                        }else{
-                            System.out.println("Numero " + numero + "no es primo");
-                            System.out.println("Numero " + numero + "no es primo");
-                            String respuesta = "<html><h1>El número " + numero + " es primo</h1>></html>";
-
-                            salida.println("HTTP/1.1 200 OK");
-                            salida.println("Server: Primo");
-                            salida.println("Date: " + new Date());
-                            salida.println("Content-type: text/html; charset=utf-8");
-                            salida.println("Content-length: " + respuesta.length());
-                            salida.println();
-                            salida.flush();//Garantizamos que el encabezado va
-                            salida.println(respuesta);
-                            salida.flush();
-                        }
-
-                        salidaSocket.close();
-                        entradaSocket.close();
-                        conexion[i].close();
-
+                    // Si al menos un servidor devolvio la palabra DIVIDE el número no es primo
+                    if (primo > 0) {
+                        System.out.println("Numero " + numero + "no es primo");
+                        String respuesta = "<html><h1>El número " + (numero*2) + " no es primo</h1>></html>";
+                        salida.println("HTTP/1.1 200 OK");
+                        salida.println("Server: Primo");
+                        salida.println("Date: " + new Date());
+                        salida.println("Content-type: text/html; charset=utf-8");
+                        salida.println("Content-length: " + respuesta.length());
+                        salida.println();
+                        salida.flush();//Garantizamos que el encabezado va
+                        salida.println(respuesta);
+                        salida.flush();
+                    } else {
+                        System.out.println("Numero " + numero + " es primo");
+                        String respuesta = "<html><h1>El número " + numero + " es primo</h1>></html>";
+                        salida.println("HTTP/1.1 200 OK");
+                        salida.println("Server: Primo");
+                        salida.println("Date: " + new Date());
+                        salida.println("Content-type: text/html; charset=utf-8");
+                        salida.println("Content-length: " + respuesta.length());
+                        salida.println();
+                        salida.flush();//Garantizamos que el encabezado va
+                        salida.println(respuesta);
+                        salida.flush();
                     }
-
-
-
-                }else{
+                } else {
                     salida.println("HTTP/1.1 404 File Not Found");
                     salida.println("Server: Primo");
                     salida.println("Date: " + new Date());
@@ -122,26 +106,49 @@ public class ServidorHttp {
                     salida.println();
                     salida.flush();
                 }
-            }catch (Exception ex){
+            } catch (Exception ex) {
                 System.err.println(ex.getMessage());
             }
         }
 
-        public static String getNumero(String peticion){
+        /**
+         * Método que obtiene el numero de los parametros
+         *
+         * @param peticion petición http que se realiza
+         * @return el numero
+         */
+        public static String getNumero(String peticion) {
             StringTokenizer tokens = new StringTokenizer(peticion, "?");
             String req_a = tokens.nextToken();
             String req = tokens.nextToken();
-            String params = req.substring(0, req.indexOf(" "))+"\n";
-            String numero = params.substring(params.indexOf("=")+1, req.indexOf(" "));
+            String params = req.substring(0, req.indexOf(" ")) + "\n";
+            String numero = params.substring(params.indexOf("=") + 1, req.indexOf(" "));
 
             System.out.println("Numero: " + numero);
             return numero;
         }
+
+        /**
+         * Función que lee una cadena de bytes mandada desde el servidor
+         *
+         * @param f        el flujo de entrada
+         * @param b        el arreglo de bytes
+         * @param posicion la posicion del arreglo
+         * @param longitud la longitud del arreglo
+         * @throws Exception
+         */
+        public static void read(DataInputStream f, byte[] b, int posicion, int longitud) throws Exception {
+            while (longitud > 0) {
+                int n = f.read(b, posicion, longitud);
+                posicion += n;
+                longitud -= n;
+            }
+        }
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         ServerSocket servidor = new ServerSocket(8081);
-        for(;;){
+        for (; ; ) {
             Socket conexion = servidor.accept();
             new Worker(conexion).start();
         }
